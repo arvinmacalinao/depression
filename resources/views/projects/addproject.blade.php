9df3572a59935216f662da7d5af1b58e6753faf9
amacalinao <arvinmacalinao001@gmail.com>
2022-03-02 16:05:07 +0800

    1st commit

---
+++ resources/views/projects/addproject.blade.php
@@ -0,0 +1,18 @@
+@extends('./layouts.app')
+
+@section('content')
+<form action="">
+<div class="container-fluid">
+<label className="control-label">Project Code *</label>
+
+
+
+    
+</div>
+</form>
+
+
+
+
+
+@endsection()
