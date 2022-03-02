faec32f980a29de7673c5eecc3bbc79a3b8aafb5
rafaeloomana <rafaelo.warde.omana@gmail.com>
2022-03-02 16:54:47 +0800

    added google map

---
+++ app/Http/Controllers/MapDataController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MapDataController extends Controller
+{
+    public function showHome()
+    {
+        return view('home.home');
+    }
+}
--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,5 +1,10 @@
 @extends('./layouts.app')
 
 @section('content')
-Hello
+    <div id="map" style="height: 660px; width: 100%;"></div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary"><h4 class="text-light">Legends</h4></nav>
+
+
+
+
 @endsection()
