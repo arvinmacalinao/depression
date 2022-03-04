7d45c3a2a335bb5d123b914c564ba1ad271e372e
rafaeloomana <rafaelo.warde.omana@gmail.com>
2022-03-04 08:31:09 +0800

    added arguments for sectors and status icons

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -51,9 +51,70 @@
                  icon1 = "{{ asset('images/markers/prj_5.png') }}"
             }
 
+            //---------------Sectors---------------------------//
+            if(value.sec_id == '1' ){ //Food Processing
+                 icon2 = "{{ asset('images/markers/sec_0.png') }}"
+            }
+
+            if(value.sec_id == '2' ){ //Furniture
+                 icon2 = "{{ asset('images/markers/sec_1.png') }}"
+            }
+
+            if(value.sec_id == '3' ){ //Gifts / Decors / Handicrafts
+                 icon2 = "{{ asset('images/markers/sec_2.png') }}"
+            }
+
+            if(value.sec_id == '4' ){ //Metals & Engineering
+                 icon2 = "{{ asset('images/markers/sec_3.png') }}"
+            }
+
+            if(value.sec_id == '5' ){ //Agriculture / Marine / Agriculture / Forestry / Livestock
+                 icon2 = "{{ asset('images/markers/sec_4.png') }}"
+            }
+
+            if(value.sec_id == '6' ){ //Health & Wellness Products
+                 icon2 = "{{ asset('images/markers/sec_5.png') }}"
+            }
+
+            if(value.sec_id == '7' ){ //ICT
+                 icon2 = "{{ asset('images/markers/sec_6.png') }}"
+            }
+
+            if(value.sec_id == '8' ){ //Halal Products & Service
+                 icon2 = "{{ asset('images/markers/sec_7.png') }}"
+            }
+
+            if(value.sec_id == '9' ){ //Other Regional Insdustry Priorites
+                 icon2 = "{{ asset('images/markers/sec_8.png') }}"
+            }
+
+            //---------------Status---------------------------//
+            if(value.stat_id == '1' ){ //Completed
+                 icon3 = "{{ asset('images/markers/stat_0.png') }}"
+            }
+
+            if(value.stat_id == '2' ){ //On-going
+                 icon3 = "{{ asset('images/markers/stat_1.png') }}"
+            }
+
+            if(value.stat_id == '3' ){ //New
+                 icon3 = "{{ asset('images/markers/stat_2.png') }}"
+            }
+
+            if(value.stat_id == '4' ){ //Graduated
+                 icon3 = "{{ asset('images/markers/stat_3.png') }}"
+            }
+
+            if(value.stat_id == '5' ){ //Widthdrawn
+                 icon3 = "{{ asset('images/markers/stat_4.png') }}"
+            }
+
+            if(value.stat_id == '6' ){ //Terminated
+                 icon3 = "{{ asset('images/markers/stat_5.png') }}"
+            }
             
             
-            mymap.addMarker({
+            mymap.addMarker({ // Marker for Project Type
                 lat: value.prj_lat,
                 lng: value.prj_long,
                 icon: icon1,
@@ -68,10 +129,10 @@
                 }
             });
 
-            mymap.addMarker({
+            mymap.addMarker({ // Marker for Sector Type
                 lat: value.prj_lat,
                 lng: value.prj_long,
-                icon: "{{ asset('images/markers/reg_5.png') }}",
+                icon: icon2,
                 infoWindow: {
                     content: value.project_name,
                 },
@@ -83,10 +144,11 @@
                 }
             });
 
-            mymap.addMarker({
+
+            mymap.addMarker({ // Marker for Status Type
                 lat: value.prj_lat,
                 lng: value.prj_long,
-                icon: "{{ asset('images/markers/sec_4.png') }}",
+                icon: icon3,
                 infoWindow: {
                     content: value.project_name,
                 },
@@ -97,22 +159,6 @@
                     this.infoWindow.close();
                 }
             });
-
-            mymap.addMarker({
-                lat: value.prj_lat,
-                lng: value.prj_long,
-                icon: "{{ asset('images/markers/stat_0.png') }}",
-                infoWindow: {
-                    content: value.project_name,
-                },
-                mouseover: function() {
-                    this.infoWindow.open(this.mymap, this);
-                },
-                mouseout: function() {
-                    this.infoWindow.close();
-                }
-            });
-
         });
     </script>
     <nav class="navbar navbar-expand-md navbar-dark bg-primary"><h4 class="text-light">Legends</h4></nav>
