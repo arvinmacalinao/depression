216fb89835403aa8a284f8bedbe4f447a9dc5470
amacalinao <arvinmacalinao001@gmail.com>
2022-03-08 16:45:40 +0800

    new nav bar and fix table of projects

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,48 +7,96 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="http://maps.google.com/maps/api/js?key=AIzaSyCMDx-ejfVStxIBhfqtBuLj98OV79kqbdY"></script>
+        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.24/gmaps.js"></script>
         <title>Depression</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
         <link href="{{ asset('css/form.css') }}" rel="stylesheet"/>
         <script src="{{ asset('js/map.js') }}"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </head>
     <body>
-<nav class="navbar navbar-expand-md navbar-dark bg-primary">
-  <!-- Brand -->
-    <a class="navbar-brand" href="#">
-      <img src="{{URL::asset('/images/brand_white.png')}}" width="30" height="30" alt="">
-    </a>
- 
-  <!-- Toggler/collapsibe Button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
- 
-  <!-- Navbar links -->
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
- 
-      <li class="nav-item">
-        <a class="nav-link" href="#" style="color: #FFFFFF"><i class="fa-solid fa-chart-column"></i>   Project Summaries</a>
-      </li>
- 
-      <li class="nav-item">
-        <a class="nav-link" href="#" style="color: #FFFFFF"><i class="fa-solid fa-list"></i>  Projects</a>
-      </li>
-    </ul>
- 
-    <form class="form-inline my-2 my-lg-0 ml-3">
-    <div class="input-group">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1"><i class="fa-solid fa-magnifying-glass"></i></span>
-      </div>
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-      </div>
-  </div>
-</nav>
+    <nav class="navbar navbar-expand-md navbar-dark">
+          <a class="navbar-brand" href="#"></a>
+          <img src="{{URL::asset('/images/brand_white.png')}}" width="25" height="25" alt="">
+      
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link disabled" href="#"> </a>
+            </li>
+
+            <li class="nav-item active">
+              <a class="nav-link" href="#"><i class="bi bi-bar-chart-steps"></i> Project Summaries</span></a>
+            </li>
+            
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Projects</span></a>
+            </li>
+
+            <li class="nav-item">
+              <form class="form-inline my-2 my-lg-0 ml-3">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
+                    </div>
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
+                    </div>
+                </form>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link disabled" href="#"> </a>
+            </li>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="bi bi-bar-chart-line-fill"></i>
+                BSC
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+            
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Projects</span></a>
+            </li>
+
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Others</span></a>
+            </li>
+
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Contacts</span></a>
+            </li>
+            
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Contacts</span></a>
+            </li>
+
+            <li class="nav-item active">
+              <a class="nav-link" href="#"></span></a>
+            </li>
+
+            <li class="nav-item active">
+              <a class="nav-link" href="#"></span></a>
+            </li>
+
+            
+
+          </ul>
+          
+  </nav>
         @yield('content')
     </body>
     <footer class="page-footer" id="footer" style="font-family: 'Roboto Condensed', sans-serif;">
@@ -62,9 +110,11 @@
               Powered by DOST CALABARZON · MIS Unit
          </div>
      </div>
- </footer>
-  <script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMDx-ejfVStxIBhfqtBuLj98OV79kqbdY&callback=initMap&libraries=&v=weekly"
-      async
+    </footer>
+     <script
+         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMDx-ejfVStxIBhfqtBuLj98OV79kqbdY&callback=initMap&libraries=&v=weekly"
+         async
     ></script>
 </html>
+
+
--- resources/views/projects/projects.blade.php
+++ resources/views/projects/projects.blade.php
@@ -8,34 +8,84 @@
             <div class="pull-right">
             <a id="print-list-btn" name="print-list-btn" class="btn btn-success btn-sm" href="" title="Print List"><span class="fa fa-print"></span> Print</a>
             <a id="download-list-btn" name="download-list-btn" class="btn btn-success btn-sm" href="" title="Download List"><span class="fa fa-floppy-o"></span> Download</a>
-            <a class="btn btn-primary btn-sm" href="./" title="Add Projects"><span class="fa fa-plus"></span> Add Projects</a>
+            <a class="btn btn-primary btn-sm" href="./addproject" title="Add Projects"><span class="fa fa-plus"></span> Add Projects</a>
             </div>
         </div>
-        <div class="card-body">
-            <div class="row">
+        <div class="row">
                 <div class="col-md-12">
-                    <table class="table table-bordered">
-                        <tr>
-                            <th>#</th>
-                            <th>Code</th>
-                            <th>Project</th>
-                            <th>Type</th>
-                        </tr>
-                        @foreach($psi_project as $data)
-                        <tr>
-                            <td>{{$data->prj_id}}</td>
-                            <td>{{$data->prj_title}}</td>
-                            <td>{{$data->prj_code}}</td>
-                            <td>{{$data->prj_code}}</td>
-                        </tr>
-                        @endforeach
-                    </table>
+                    <table class="table table-bordered table-hover">
+                        <thead class="thead-proj">
+                            <tr class="">
+                                <th></th>
+                                <th>#</th>
+                                <th>Code</th>
+                                <th>Project</th>
+                                <th>Type</th>
+                                <th>Year Approved</th>
+                                <th>Beneficiaries</th>
+                                <th>Collaborators</th>
+                                <th>Sector</th>
+                                <th>Region</th>
+                                <th>Province</th>
+                                <th>District</th>
+                                <th>Status</th>
+                                <th>Project Cost</th>
+                                <th>Amount Due</th>
+                                <th>Refunded</th>
+                                <th>Refund Rate</th>
+                                <th>Implementor </th>
+                            </tr>
+                    </thead>
+                            @foreach($psi_project as $data)
+                            <tr>
+                                <td></td>
+                                <td class="counterCell"></td>
+                                <td>{{$data->prj_id}}</td>
+                                <td>{{$data->prj_title}}</td>
+                                <td>{{$data->prj_type_name}}</td>
+                                <td>{{$data->prj_year_approved}}</td>
+                                <td>{{$data->coop_names}}</td>
+                                <td>{{$data->collaborator_names}}</td>
+                                <td>{{$data->sector_name}}</td>
+                                <td>{{$data->region_code}}</td>
+                                <td>{{$data->province_name}}</td>
+                                <td>{{$data->city_name}}</td>
+                                <td>{{$data->district_name}}</td>
+                                <td>{{$data->prj_status_name}}</td>
+                                <td>Php {{ $data->prj_cost_setup}}</td>
+                                <td>{{$data->rep_total_due}}</td>
+                                <td>{{$data->rep_refund_rate}}</td>
+                                <td>{{$data->ug_name}}</td>
+                            </tr>
+                            @endforeach
+                        </table>
                 </div>
             </div>
             
         </div>
+        <div class="card-footer">
+        <table class="table table-condensed">
+            <thead>
+            <tr>
+                <th class="text-center">Total Project Cost</th>
+                <th class="text-center">Total Amount Due</th>
+                <th class="text-center">Total Amount Refunded</th>
+                <th class="text-center">Refund Rate</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td class="text-center">-</td>
+                <td class="text-center">-</td>
+                <td class="text-center">-</td>
+                <td class="text-center">-</td>
+            <tr>
+            </tbody>
+        </table>
+        </div>
     </div>
-</div>
+    </div>
+
 
 
 @endsection()
